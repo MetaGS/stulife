b1a4b7807060099e7021514dc6739b2332b7e9e4
Chee Chin Wei <chinwei.chee@gmail.com>
2015-12-06 01:58:56 +0800

    added locale

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
-  devise_for :admins, path: "/admin"
+  devise_for :users, only: :omniauth_callbacks, controllers: {omniauth_callbacks: 'users/omniauth_callbacks'}
 
-  root to: 'pages#home'
+  scope "(:locale)", locale: /en|cn|my/ do
+    devise_for :users, skip: :omniauth_callbacks
+    devise_for :admins, path: "/admin"
+
+    root to: 'pages#home'
+  end
 
   namespace :admin do
     root to: 'admin#dashboard'
