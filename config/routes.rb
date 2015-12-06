85f23a4c63e81faaaa73f07e69153337d1be3767
Chee Chin Wei <chinwei.chee@gmail.com>
2015-12-06 16:42:12 +0800

    changed users devise path

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, only: :omniauth_callbacks, controllers: {omniauth_callbacks: 'users/omniauth_callbacks'}
 
   scope "(:locale)", locale: /en|cn|my/ do
-    devise_for :users, skip: :omniauth_callbacks
+    devise_for :users, skip: :omniauth_callbacks, path: "/"
     devise_for :admins, path: "/admin"
 
     root to: 'pages#home'
