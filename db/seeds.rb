a9d7408ddcede949ce8fe43732dd812b6ba9e0b4
Chee Chin Wei <chinwei.chee@gmail.com>
2015-12-06 12:26:39 +0800

    added new admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,3 @@
 Admin.create(email: "chinwei.chee@gmail.com", password: "random1234", password_confirmation: "random1234")
+Admin.create(email: "soogarjun@gmail.com", password: "random1234", password_confirmation: "random1234")
+Admin.create(email: "sohcheehong96@gmail.com", password: "random1234", password_confirmation: "random1234")
