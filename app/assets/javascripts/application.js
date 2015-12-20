78ec416a5a9390936452f06f9796aa68b58d8d32
Chee Chin Wei <chinwei.chee@gmail.com>
2015-12-20 20:59:04 +0800

    completed frontend

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,17 +1,26 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap
+//= require plugins/jquery.appear/jquery.appear.min
+//= require plugins/jquery.easing/jquery.easing.min
+//= require plugins/jquery-cookie/jquery-cookie.min
+//= require plugins/common/common.min
+//= require plugins/jquery.validation/jquery.validation.min
+//= require plugins/jquery.stellar/jquery.stellar.min
+//= require plugins/jquery.easy-pie-chart/jquery.easy-pie-chart.min
+//= require plugins/jquery.gmap/jquery.gmap.min
+//= require plugins/jquery.lazyload/jquery.lazyload.min
+//= require plugins/isotope/jquery.isotope.min
+//= require plugins/owl.carousel/owl.carousel.min
+//= require plugins/magnific-popup/jquery.magnific-popup.min
+//= require plugins/vide/vide.min
+
+//= require theme
+//= require plugins/rs-plugin/js/jquery.themepunch.tools.min
+//= require plugins/rs-plugin/js/jquery.themepunch.revolution.min
+//= require plugins/circle-flip-slideshow/js/jquery.flipshow.min
+//= require views/view.home
+//= require theme.init
+
 // require_tree .
