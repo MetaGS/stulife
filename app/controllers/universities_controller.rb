2354b7d4d2778f424713ca3c78bebe86ccbcff7f
Chee Chin Wei <chinwei.chee@gmail.com>
2015-12-21 10:33:10 +0800

    added university show views page

--- app/controllers/universities_controller.rb
+++ app/controllers/universities_controller.rb
@@ -2,4 +2,8 @@ class UniversitiesController < ApplicationController
   def index
     @universities = University.where(country: @country)
   end
+
+  def show
+    @university = University.friendly.find(params[:id])
+  end
 end
